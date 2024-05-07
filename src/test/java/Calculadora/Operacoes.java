8d167f880eee0ac445a318fa847202a903a14a3a
Marcos <marcos_goncalves12@icloud.com>
2024-05-06 22:52:11 -0300

    ajuste de quantidade de numeros

--- src/test/java/Calculadora/Operacoes.java
+++ src/test/java/Calculadora/Operacoes.java
@@ -3,8 +3,8 @@ package Calculadora;
 //class de page 
 public class Operacoes {
 	
-	public void soma (int x1, int x2) {
-		int resultado = x1+x2;
+	public void soma (int x1, int x2, int x3) {
+		int resultado = x1+x2+ x3;
 		System.out.println("O resultado da sua soma é " + resultado);
 				
 	}
