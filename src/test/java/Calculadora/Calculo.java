6cd78035e0fba67a59f9adbb626f795d194d4e4b
Marcos <marcos_goncalves12@icloud.com>
2024-05-06 22:35:00 -0300

    codigo de comandos basicos

---
+++ src/test/java/Calculadora/Calculo.java
@@ -0,0 +1,15 @@
+package Calculadora;
+
+public class Calculo {
+	
+	//class execução
+	public static void main(String[] args) {
+		Operacoes operacoes = new Operacoes();
+		
+		operacoes.soma(80, 36);
+		operacoes.multiplicacao(26, 79);
+		operacoes.divisao(25, 12);
+		operacoes.subtracao(79, 25);
+	}
+
+}
---
+++ src/test/java/Primeiroscomandos.java
@@ -0,0 +1,15 @@
+
+public class Primeiroscomandos {
+
+	public static void main(String[] args) {
+		
+		String  nome   = "Marcos";
+		int     idade  =  27;
+		double  altura =  1.67;
+		
+		System.out.println("Meu nome é " + nome + ", minha idade é " + idade + " anos"  +  " e minha altura é de " + altura + "!");
+		 
+		
+	}
+
+}
